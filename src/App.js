9513678283248b8d83c44b90758bfe14e758d768
luisbryandr <luisbdominguezr@gmail.com>
2021-08-20 12:41:43 -0400

    Built out preliminary Home and Pizza form componets with their working links and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 
+function Pizza (){
+  return <h1>Live your best life, create your ideal pizza below</h1>
+}
+function Home (){
+  return <h1>Back at Home eh?</h1>
+}
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <BrowserRouter>
+        <Link to= '/'>Home</Link>
+        <Link to= '/pizza'>Pizza?</Link>
+
+        <Route exact path ='/' component={Home} />
+        <Route path='/pizza' component={Pizza} />
+        
+      </BrowserRouter>
+      
     </>
   );
 };
