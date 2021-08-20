029dd8b80564bf10940c4fe675c215d51b997a86
luisbryandr <luisbdominguezr@gmail.com>
2021-08-20 13:01:25 -0400

    started and rendered form on Pizza component, with correct ids

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import React from "react";
 import {BrowserRouter, Route, Link} from 'react-router-dom'
 
 function Pizza (){
-  return <h1>Live your best life, create your ideal pizza below</h1>
+  return (
+  <div>
+    <h1>Live your best life, create your ideal pizza below</h1>
+    <form id='pizza-form'>
+      <label htmlFor='name-input'>Coder's Name</label>
+      <input
+        maxLength='16'
+        placeholder='Your name goes here'
+        id='name-input'
+        name='fname'
+        type='text'
+      /><br />
+      <select>
+        <option>xxxxx</option>
+        <option>yyyy</option>
+        <option>zzzz</option>
+
+      </select>
+    </form>
+  </div>
+  )
 }
 function Home (){
   return <h1>Back at Home eh?</h1>
@@ -12,12 +32,12 @@ const App = () => {
     <>
       <h1>Lambda Eats</h1>
       <BrowserRouter>
-        <Link to= '/'>Home</Link>
-        <Link to= '/pizza'>Pizza?</Link>
+        <Link to= '/'>Home </Link>
+        <Link to= '/pizza'> Pizza?</Link>
 
         <Route exact path ='/' component={Home} />
         <Route path='/pizza' component={Pizza} />
-        
+
       </BrowserRouter>
       
     </>
