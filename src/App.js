060eaf899529fa45d722118bffe7299cb028ee36
luisbryandr <luisbdominguezr@gmail.com>
2021-08-20 19:31:57 -0400

    learned and implemented handling form input change as a change in state

--- src/App.js
+++ src/App.js
@@ -1,62 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import {BrowserRouter, Route, Link} from 'react-router-dom'
 
-function Pizza (){
-  return (
-  <div>
-    <h1>Live your best life!</h1>
-      <h3> Create your ideal pizza below</h3>
-    <form id='pizza-form'>
-      <label htmlFor='name-input'>Coder's Name</label>
-      <input
-        maxLength='16'
-        placeholder='Your name goes here'
-        id='name-input'
-        name='fname'
-        type='text'
-      /><br />
-      <label htmlFor='size-dropdown'>Size of your Pie!</label>
-      <select id='size-dropdown'>
-        <option value='1'>Personal / Small</option>
-        <option value='2'>For Two / Medium</option>
-        <option value='3'>To Share / Large</option>
-      </select><br />
-
-      <label>Pizza Toppings
-        <input
-          type='checkbox'
-          id='toppings-checklist'
-          name='toppings'
-          />
-          <input
-          type='checkbox'
-          id='toppings-checklist'
-          name='pineapple'/>
-
-      </label><br />
+import Pizza from "./PizzaForm";
 
 
-      <label htmlFor='special-text'>Got a particular way you like your pie?</label>
-      <input
-      type='text'
-      id='special-text'
-      maxLength='75'
-      placeholder='Special instructions go here'
-      name='specialinstructions'
-      /><br />
-      <input type='submit'/>
-      <button id='order-button'>Add to Order</button>
-    </form>
-  </div>
-  )
-
-   //revisit this checkbox - you need a checklist for toppings!
-   
-}
 function Home (){
   return <h1>Back at Home, eh?</h1>
 }
 const App = () => {
+
+  const [input, setInput] = useState('');
+
+  const handleChange = event =>{
+    setInput(event.target.value);
+  };
+  const handleSubmit = () => {
+    console.log(input);
+  };
   return (
     <>
       <h1>Lambda Eats</h1>
---
+++ src/PizzaForm.js
@@ -0,0 +1,78 @@
+import React, {useState} from 'react';
+
+
+
+export default function Pizza (props){
+    const [formInput, setFormInput] = useState({
+      name: '',
+      size: '',
+      pineapple: false,
+      tomato: false,
+      spinach: false,
+      love: false,
+      special: '',
+    })
+    const onInputChange = event => {
+      setFormInput({
+        ...formInput,[event.target.name]: event.target.value,
+      })
+    }
+
+    return (
+    <div>
+        <div>
+          <h1>Live your best life!</h1>
+            <img src='../assets/pizza'/>
+            <h3> Create your ideal pizza below</h3>
+        </div>
+      <form id='pizza-form'>
+        <label htmlFor='name-input'>Coder's Name</label>
+        <input
+          maxLength='16'
+          placeholder='Your name goes here'
+          id='name-input'
+          name='name'
+          type='text'
+          onChange={onInputChange}
+        /><br />
+        <label htmlFor='size-dropdown'>Size of your Pie!</label>
+        <select id='size-dropdown'name='size'onChange={onInputChange}>
+          <option value='1'>Personal / Small</option>
+          <option value='2'>For Two / Medium</option>
+          <option value='3'>To Share / Large</option>
+        </select><br />
+  
+        <label>Pizza Toppings
+          <input
+            type='checkbox'
+            id='toppings-checklist'
+            name='toppings'
+            onChange={onInputChange}
+            />
+            <input
+            type='checkbox'
+            id='toppings-checklist'
+            name='pineapple'
+            onChange={onInputChange}
+            />
+  
+        </label><br />
+  
+  
+        <label htmlFor='special-text'>Got a particular way you like your pie?</label>
+        <input
+        type='text'
+        id='special-text'
+        maxLength='75'
+        placeholder='Special instructions go here'
+        name='special'
+        onChange={onInputChange}
+        /><br />
+        <button id='order-button'>Add to Order</button>
+      </form>
+    </div>
+    )
+  
+     //revisit this checkbox - you need a checklist for toppings!
+     
+  }
